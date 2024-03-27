25d6f8e57d9575c2d97dab0a9cc630eeae4efcd5
Jully Soares <jullyensoares@hotmail.com>
2024-03-27 18:01:17 -0300

    Classes TabelaFipe

--- extract.py
+++ extract.py
@@ -1,9 +1,25 @@
 import pandas as pd
 
-df = pd.read_excel('./zap.xlsx')
-df.to_csv('FIPE.csv', index=False)
 
-fileCSV = pd.read_csv('./FIPE.csv')
-print(fileCSV)
+class TabelaFipe():
+    def __init__(self) -> None:
+        self.df=pd.read_excel('./zap.xlsx')
+        self.df.to_csv('FIPE.csv', index=False)
+        self.fileCSV = pd.read_csv('./FIPE.csv')
+
+    def qtdElementos(self):
+        return len(self.df)
+    
+    def separacaocolumns(self):
+        df = pd.DataFrame(eval('./FIPE.csv'))
+        duas_colunas = df[['Data', 'Total']]
+        return duas_colunas
+
+        
+
+
+tabela = TabelaFipe()
+print("Quantidade de elementos:", tabela.qtdElementos())
+print("Coluna data: ", tabela.separacaocolumns())
 
 
